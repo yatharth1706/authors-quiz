0254738bd52369da2912615fa4a40318d80d7cab
Yatharth Verma <Yatharth.Verma@lsq.net>
2020-07-27 11:46:13 +0530

    Add author quiz component

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function AuthorQuiz() {
+  return (
+    <div className="App">
+      <h2>Author Quiz</h2>
+    </div>
+  );
+}
+
+export default AuthorQuiz;
