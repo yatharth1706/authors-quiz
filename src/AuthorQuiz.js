222d44376b86fb2f1262826177796aa6bff90f2a
Yatharth Verma <Yatharth.Verma@lsq.net>
2020-07-27 12:25:35 +0530

    Fixing home page

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -3,10 +3,67 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function AuthorQuiz() {
+function Hero() {
+  return (
+    <div className="row"> 
+      <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book written by the author shown</p>
+      </div>
+    </div>
+  )
+}
+
+
+function Book({title}) {
+  return (
+    <div className="answer">
+      <h4>{title}</h4>
+    </div>
+  )
+}
+
+function Turn({author, books}) {
+  return (
+    <div className="row turn" style={{backgroundColor : "white"}}>
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="authorimage" alt="author" />
+      </div>
+      <div className="col-6">
+  {books.map((title,index) => <Book title={title} key={title}/>)}
+      </div>
+    </div>
+  )
+}
+
+function Continue() {
+  return (
+    <div></div>
+  )
+}
+
+
+function Footer() {
+  return (
+    <div id="footer" className="row">
+      <div className="col-12">
+        <p className="text-muted credit">
+          All images are from <a href="http://commons.wikimedia.org/wiki">Wikimedia commons</a>
+        </p>
+      </div>  
+    </div>
+  )
+}
+
+function AuthorQuiz({turnData}) {
   return (
     <div className="App">
-      <h2>Author Quiz</h2>
+      <div className = "container-fluid">
+        <Hero />
+        <Turn {...turnData}/>
+        <Continue />
+        <Footer />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,25 @@ import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
 
+const authors = [
+  {
+    name : 'Mark Twain',
+    imageUrl : 'images/authors/marktwain.jfif',
+    imageSource : 'Wikimedia Commons',
+    books: ['The Adventures of Huckleberry Finn', 'Life in the Mississippie', 'Roughing it']
+  }
+]
+
+const state = {
+  turnData: {
+    author : authors[0],
+    books:  authors[0].books
+  }
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <AuthorQuiz />
+    <AuthorQuiz {...state}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
